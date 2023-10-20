0351972a881b99cb65d86b826ceeedc183ec6b74
weedge <weege007@gmail.com>
2023-10-20 18:03:04 +0800

    change run_redisxann.sh
    Signed-off-by: weedge <weege007@gmail.com>

--- scripts/run_redisxann.sh
+++ scripts/run_redisxann.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 rm -rf redis && git clone https://github.com/redis/redis && \
   cd redis && \
   git checkout 7.0 && \
-  make REDIS_CFLAGS='-Werror' BUILD_TLS=yes && make install && \
-  which redis-server && redis-server --version
+  make REDIS_CFLAGS='-Werror' BUILD_TLS=yes && make install && cd - \
+  && which redis-server && redis-server --version
 
 curl https://sh.rustup.rs -sSf > rustup.sh
 sh rustup.sh -y
@@ -12,7 +13,8 @@ sh rustup.sh -y
 git clone https://github.com/weedge/RedisXANN.git --recursive
 cd RedisXANN && source "$HOME/.cargo/env" && \
   cargo clean && \
-  cargo build --lib --manifest-path rust/usearch/Cargo.toml --release
+  cargo build --lib --manifest-path rust/usearch/Cargo.toml --release \
+  && cd -
 
 redis-server --daemonize yes \
   --loadmodule RedisXANN/target/release/libredisxann_usearch.so is_remove_serialized_file 1 \
